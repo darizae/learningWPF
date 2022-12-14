085b676e295ae886d3b2ac89bb00195a16bdc19a
darizaec <danielarizae@gmail.com>
2022-12-14 22:04:11 +0100

    Make Basic Calculator in WPF

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,74 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string strZahl;
+        private string strLetzteOperation;
+        private double dblErgebnis;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            strZahl = "";
+            strLetzteOperation = "";
+            dblErgebnis = 0.0;
+
+
+        }
+
+        private void btn_eq_Click(object sender, RoutedEventArgs e)
+        {
+            strZahl = eingabe_tf.Text;
+            double eingabe = Double.Parse(strZahl);
+            berechnen(eingabe);
+            updateGUI();
+        }
+
+        private void btn_c_Click(object sender, RoutedEventArgs e)
+        {
+            resetGUI();
+        }
+
+        public void berechnen(double value)
+        {
+            if (rb_root.IsChecked == true)
+            {
+                dblErgebnis = Math.Sqrt(value);
+            }
+
+            if (rb_pot.IsChecked == true)
+            {
+                dblErgebnis = Math.Exp(value);
+            }
+
+            if (rb_sin.IsChecked == true)
+            {
+                dblErgebnis = Math.Sin(value);
+            }
+
+            if (rb_tan.IsChecked == true)
+            {
+                dblErgebnis = Math.Tan(value);
+            }
+            
+            strLetzteOperation += dblErgebnis;
+        }
+
+        public void resetGUI()
+        {
+            list_box.Items.Clear();
+            eingabe_tf.Text = "";
+            ausgabe_lb.Content = "0.0";
+            dblErgebnis = 0;
+        }
+
+        public void updateGUI()
+        {
+            list_box.Items.Add(dblErgebnis);
+            ausgabe_lb.Content = dblErgebnis;
+            eingabe_tf.Text = dblErgebnis.ToString();
         }
     }
+
 }
