1bd7cdd8080c07adb736254b95d1e1414e338a53
darizaec <danielarizae@gmail.com>
2022-12-19 21:31:28 +0100

    Implement toString method

--- Wandrechner/Wandrechner/Wand.cs
+++ Wandrechner/Wandrechner/Wand.cs
@@ -11,7 +11,7 @@ namespace Wandrechner
         public double laenge, breite, hoehe;
         public string material;
         public bool istTragend;
-        public double bewehrungsmenge, schalungsFlaeche;
+        public double bewehrungsmenge, schalungsmenge;
 
         public Wand()
         {
@@ -21,18 +21,18 @@ namespace Wandrechner
             this.material = "";
             this.istTragend = false;
             this.bewehrungsmenge = 0.0;
-            this.schalungsFlaeche = 0.0;
+            this.schalungsmenge = 0.0;
         }
 
         public void BerechneSchalungsflaeche()
         {
             if (this.material.Equals("Mauerwerk"))
             {
-                this.schalungsFlaeche = laenge * hoehe * 2;
+                this.schalungsmenge = laenge * hoehe * 2;
             }
             else
             {
-                this.schalungsFlaeche = 0.0;
+                this.schalungsmenge = 0.0;
             }
         }
 
@@ -55,6 +55,15 @@ namespace Wandrechner
             double ergebnis = this.hoehe * this.laenge * this.breite * bewehrungsgrad;
 
             this.bewehrungsmenge = Math.Round(ergebnis, 2);
+        }
+
+        public string GebeWandinfosAus()
+        {
+            return
+                $"Masse (b * l * h): {this.breite} m * {this.laenge} m * {this.hoehe} m\n" +
+                $"Material: {this.material}\n" +
+                $"Bewehrungsmenge: {this.bewehrungsmenge} \n" +
+                $"Schalungsmenge: {this.schalungsmenge} m2";
         }
     }
 }
