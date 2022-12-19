a1f9f4809f8bfcaa426db63d87910979dc695cc9
darizaec <danielarizae@gmail.com>
2022-12-19 21:13:57 +0100

    Create Wand Class along with Constructor

---
+++ Wandrechner/Wandrechner/Wand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wandrechner
+{
+    internal class Wand
+    {
+        public double laenge, breite, hoehe;
+        public string material;
+        public bool istTragend;
+        public double bewehrungsFlaeche, schalungsFlaeche;
+
+        public Wand()
+        {
+            this.laenge = 0.0;
+            this.breite = 0.0;
+            this.hoehe = 0.0;
+            this.material = "";
+            this.istTragend = false;
+            this.bewehrungsFlaeche = 0.0;
+            this.schalungsFlaeche = 0.0;
+        }
+    }
+}
