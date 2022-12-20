ac01a77a4580cd0279f1b6483548e698245bffdf
darizaec <danielarizae@gmail.com>
2022-12-20 21:26:41 +0100

    Set content of Border box

--- CurrencyConverter/CurrencyConverter/obj/Debug/CurrencyConverter_Content.g.i.cs
+++ CurrencyConverter/CurrencyConverter/obj/Debug/CurrencyConverter_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/money_icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/arrows.png")]
 
 
