00d6fe6e990b3498ec25ed6bf4c04683ec545034
darizaec <danielarizae@gmail.com>
2022-12-21 22:45:04 +0100

    Populate currency combo boxes with values from Data Table
    Creates a DataTable instance. It consists of two columns: one for the
    currency's name (short form) and another for the currency's value.
    The displayed value at the combo boxes will be that of the Name column.
    The selected value at the combo boxed will be that of Value column.

--- CurrencyConverter/CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/CurrencyConverter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,50 @@ namespace CurrencyConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable dtCurrency;
         public MainWindow()
         {
             InitializeComponent();
+
+            BuildDataTable();
+            BindCurrency(cbo_currencyFrom);
+            BindCurrency(cbo_currencyTo);
+        }
+
+        //Currency data for ComboBox
+        private void BuildDataTable()
+        {
+            //Initialize instance of DataTable
+            dtCurrency = new DataTable();
+
+            //Add columns: name of currency and its value
+            dtCurrency.Columns.Add("Name");
+            dtCurrency.Columns.Add("Value");
+
+            //Add currencies and their values
+            dtCurrency.Rows.Add("--Select--", 0);
+            dtCurrency.Rows.Add("INR", 1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR", 20);
+            dtCurrency.Rows.Add("GBP", 5);
+            dtCurrency.Rows.Add("DEM", 43);
+        }
+
+        
+        private void BindCurrency(ComboBox comboBox)
+        {
+            //ComboBox is populated with data from the DataTable
+            comboBox.ItemsSource = dtCurrency.DefaultView;
+
+            //The column values which are to be displayed in the GUI
+            comboBox.DisplayMemberPath= "Name";
+
+            //The column values that will be selected behind the scenes
+            comboBox.SelectedValuePath = "Value";
+
+            //The index of the default value ("--Select--")
+            comboBox.SelectedIndex = 0;
         }
     }
 }
